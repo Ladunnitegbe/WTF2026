8f93f4a61ae69934f6bba682e926966060a93717
Ladunnitegbe <akinsolaladunni@gmail.com>
2025-09-07 06:49:17 +0100

    My first assignment

---
+++ Week 1- September/index.js
@@ -0,0 +1,19 @@
+let num1;
+let num2;
+let name;
+
+num1 = 4;
+num2 = 7;
+name = "Jane";
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+let message;
+let sum = num1 + num2;
+
+message = `Hello ${name}, the result of ${num1} + ${num2} is ${sum}`;
+
+console.log(message);
